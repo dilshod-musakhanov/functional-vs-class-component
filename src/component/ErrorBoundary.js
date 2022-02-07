c8de50f05f763c5f38c525cd9602012e0d9c8435
Dilshod Musakhanov <musakhanov@yahoo.com>
2022-02-07 07:03:14 +0400

    difference in using setValue as separate function

--- src/component/ErrorBoundary.js
+++ src/component/ErrorBoundary.js
@@ -1,7 +1,13 @@
 import React, {useState} from "react";
 
 //difference in implementing useState
+//make setValue (setNewValue) as separate function
+//in Class component in order to use setValue as separate function
+//we have to bind it to this as each time we click onClick we lose context
+//so we have to bind setValue function to this
 
+//as you can see now hooks are allowing us to use state in Functional component which results
+//in writing less codes comparing to Class component
 
 
 //Class component
@@ -13,11 +19,16 @@ class ErrorBoundary extends React.Component {
             value: 1,
         }
     }
+    this.setNewValue = this.setNewValue.bind(this);
+
+    setNewValue() {
+        this.setState({value: this.state.value + 1});
+    }
     render() {
         return (
             <div>
                 <h1>Hello,{this.state.value}</h1>
-                <button onClick={() => this.setState({value: this.state.value + 1})}>Click</button>
+                <button onClick={this.setNewValue}>Click</button>
             </div>
         )
     }
@@ -29,10 +40,12 @@ class ErrorBoundary extends React.Component {
 //Functional component
 const ErrorB = () => {
     const [value,setValue] = useState(1)
+    const setNewValue = ()=> setValue(value + 1);
+
     return (
         <div>
             <h1>Hello,{value}</h1>
-            <button onClick={()=> setValue(value + 1)}>Click</button>
+            <button onClick={setNewValue}>Click</button>
         </div>
     )
 }
